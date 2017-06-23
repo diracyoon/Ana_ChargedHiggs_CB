2778bf0994702f8f376c4911bde76c065a12cf76
Inseok Yoon <diracyoon@gmail.com>
2017-06-23 13:26:47 +0900

    start

---
+++ src/Ana_ChargedHiggs_CB_LinkDef.h
@@ -0,0 +1,9 @@
+#ifdef __ROOTCLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class Fitter_Test+;
+
+#endif /*__ROOTCLING__*/
---
+++ src/Fitter_Test.C
@@ -0,0 +1,17 @@
+#include "Fitter_Test.h"
+
+ClassImp(Fitter_Test);
+
+//////////
+
+Fitter_Test::Fitter_Test()
+{
+}//Fitter_Test::Fitter_Test()
+
+//////////
+
+Fitter_Test::~Fitter_Test()
+{
+}//Fitter_Test::~Fitter_Test()
+
+//////////
